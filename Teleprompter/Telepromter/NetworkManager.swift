53bf61de2048d61b8250f2560ccedd751bc58651
Alex Sofonea <70516886+Alex05007@users.noreply.github.com>
2023-04-15 23:18:12 +0300

    Apps ready for launch
    Teleprompter needs fixes

---
+++ Teleprompter/Telepromter/NetworkManager.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkManager.swift
+//  Telepromter
+//
+//  Created by Alex on 15.10.2022.
+//
+
+import Foundation
+import Network
+
+class NetworkManager: ObservableObject {
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue (label: "NetworkManager")
+    @Published var isConnected = true
+    
+    var connectionDescription: String {
+        if (isConnected == true) {
+            return "true"
+        } else {
+            return "false"
+        }
+    }
+    
+    init() {
+        monitor.pathUpdateHandler = { path in
+            DispatchQueue.main.async {
+                self.isConnected = path.status == .satisfied
+            }
+        }
+        monitor.start (queue: queue)
+    }
+}
---
+++ Teleprompter/Telepromter/TelepromterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelepromterApp.swift
+//  Telepromter
+//
+//  Created by Alex on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TelepromterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
