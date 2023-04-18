f2b2e3a0bc5bfa2b829b14019b269e1850d9f858
Alex Sofonea <70516886+Alex05007@users.noreply.github.com>
2023-04-19 00:06:33 +0300

    Adding account app

---
+++ Account/Account/AccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountApp.swift
+//  Account
+//
+//  Created by Alex on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
