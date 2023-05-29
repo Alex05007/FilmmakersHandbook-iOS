d22f5aa8627eeee8509967a6b05cc27a66e63b56
Alex Sofonea <70516886+Alex05007@users.noreply.github.com>
2023-05-29 21:54:40 +0300

    Ready for release - design

--- WorkPlace/WorkPlace/ContentView.swift
+++ WorkPlace/WorkPlace/ContentView.swift
@@ -20,8 +20,8 @@ struct view : UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        /*uiView.allowsBackForwardNavigationGestures = true
-        let curentUrl = request.url!.absoluteString
+        uiView.allowsBackForwardNavigationGestures = true
+        /*let curentUrl = request.url!.absoluteString
         print(curentUrl)
         if curentUrl.contains(appName) {
             uiView.load(request)
@@ -32,73 +32,12 @@ struct view : UIViewRepresentable {
     }
 }
 
-struct BackdropView: UIViewRepresentable {
-    
-    func makeUIView(context: Context) -> UIVisualEffectView {
-        let view = UIVisualEffectView()
-        let blur = UIBlurEffect(style: .light)
-        let animator = UIViewPropertyAnimator()
-        animator.addAnimations { view.effect = blur }
-        animator.fractionComplete = 0
-        animator.stopAnimation(true)
-        animator.finishAnimation(at: .start)
-        return view
-    }
-    
-    func updateUIView(_ uiView: UIVisualEffectView, context: Context) { }
-    
-}
-
-/// A transparent View that blurs its background
-struct BackdropBlurView: View {
-    
-    let radius: CGFloat
-    let opaque: Bool
-    
-    @ViewBuilder
-    var body: some View {
-        BackdropView().blur(radius: radius, opaque: opaque)
-    }
-    
-}
-
-func getSafeAreaTop()->CGFloat{
-
-        let keyWindow = UIApplication.shared.connectedScenes
-
-            .filter({$0.activationState == .foregroundActive})
-
-            .map({$0 as? UIWindowScene})
-
-            .compactMap({$0})
-
-            .first?.windows
-
-            .filter({$0.isKeyWindow}).first
-
-        
-
-        return (keyWindow?.safeAreaInsets.top)!
-
-    }
-
-let window = UIApplication.shared.windows[0]
-let topPadding = window.safeAreaInsets.top + 26
-
 struct ContentView: View {
     @ObservedObject var isConnected = NetworkManager()
     var body: some View {
         if (isConnected.connectionDescription == "true") {
-            ZStack {
-                view(request: URLRequest(url: URL(string: "https://gnets.myds.me/filmmaker/iOSredirect.php?deviceId=" + UIDevice.current.identifierForVendor!.uuidString + "&app=" + appName)!)).previewLayout(.fixed(width: .infinity, height: .infinity))
+            view(request: URLRequest(url: URL(string: "https://gnets.myds.me/filmmaker/iOSredirect.php?deviceId=" + UIDevice.current.identifierForVendor!.uuidString + "&app=" + appName)!)).previewLayout(.fixed(width: .infinity, height: .infinity))
                     .ignoresSafeArea()
-                GeometryReader { geometry in
-                    BackdropBlurView(radius: 5, opaque: true)
-                        .frame(width: 1000, height: topPadding)
-                        .position(x: 0, y: 0)
-                        .ignoresSafeArea()
-                }
-            }
         } else {
             VStack {
                 Image(systemName: "wifi.exclamationmark")
